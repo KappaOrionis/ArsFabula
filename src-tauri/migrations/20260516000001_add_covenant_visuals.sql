5c3ca17ce2c61d72371fcdadc4d9c6f37025150e
Kappa <funnypoum@gmail.com>
2026-05-16 00:42:56 +0200

    feat: add visual_path column to covenants table and map archetype visuals

--- src-tauri/migrations/20260516000001_add_covenant_visuals.sql
+++ src-tauri/migrations/20260516000001_add_covenant_visuals.sql
@@ -3,23 +3,38 @@
 
 ALTER TABLE covenants ADD COLUMN visual_path TEXT;
 
-UPDATE covenants SET visual_path = '/covenants/cov_black_forest_citadel.png' WHERE id = 'cov-durenmar-0000';
+UPDATE covenants SET visual_path = '/covenants/cov_durenmar_citadel.png' WHERE id = 'cov-durenmar-0000';
 UPDATE covenants SET visual_path = '/covenants/cov_fengheld_promontory.png' WHERE id = 'cov-fengheld-0000';
 UPDATE covenants SET visual_path = '/covenants/cov_coeris_citadel.png' WHERE id = 'cov-coeris-0000';
 UPDATE covenants SET visual_path = '/covenants/cov_blackthorn_fortress.png' WHERE id = 'cov-blackthorn-0000';
 
-UPDATE covenants SET visual_path = '/covenants/cov_mountain_fortress.png' WHERE id IN ('cov-doissetep-0000', 'cov-horsinglas-0000');
+UPDATE covenants SET visual_path = '/covenants/cov_doissetep_fortress.png' WHERE id = 'cov-doissetep-0000';
 UPDATE covenants SET visual_path = '/covenants/cov_valnegra_ruins.png' WHERE id = 'cov-valnegra-0000';
 UPDATE covenants SET visual_path = '/covenants/cov_kastellon_pass.png' WHERE id = 'cov-kastellon-0000';
+UPDATE covenants SET visual_path = '/covenants/cov_horsinglas_broch.png' WHERE id = 'cov-horsinglas-0000';
 
-UPDATE covenants SET visual_path = '/covenants/cov_ancient_forest_shrine.png' WHERE id = 'cov-crintera-0000';
+UPDATE covenants SET visual_path = '/covenants/cov_crintera_shrine.png' WHERE id = 'cov-crintera-0000';
 UPDATE covenants SET visual_path = '/covenants/cov_ashenrise_sanctuary.png' WHERE id = 'cov-ashenrise-0000';
 UPDATE covenants SET visual_path = '/covenants/cov_krasnorechye_ostrog.png' WHERE id = 'cov-krasnorechye-0000';
 
-UPDATE covenants SET visual_path = '/covenants/cov_faerie_glade_palace.png' WHERE id = 'cov-irencilia-0000';
-UPDATE covenants SET visual_path = '/covenants/cov_coastal_storm_fortress.png' WHERE id IN ('cov-fudarus-0000', 'cov-exspectatio-0000', 'cov-semitae-0000');
-UPDATE covenants SET visual_path = '/covenants/cov_roman_villa_estate.png' WHERE id IN ('cov-harco-0000', 'cov-favras-0000', 'cov-magvillus-0000');
-UPDATE covenants SET visual_path = '/covenants/cov_magical_forge_cavern.png' WHERE id IN ('cov-verdi-0000', 'cov-twisting-0000');
-UPDATE covenants SET visual_path = '/covenants/cov_renaissance_manor.png' WHERE id IN ('cov-levrier-0000', 'cov-valnastium-0000', 'cov-pythagoranis-0000');
-UPDATE covenants SET visual_path = '/covenants/cov_moorish_palace.png' WHERE id IN ('cov-jaferia-0000', 'cov-castrasolis-0000');
-UPDATE covenants SET visual_path = '/covenants/cov_crusader_crypt.png' WHERE id = 'cov-acre-0000';
+UPDATE covenants SET visual_path = '/covenants/cov_irencilia_palace.png' WHERE id = 'cov-irencilia-0000';
+
+UPDATE covenants SET visual_path = '/covenants/cov_fudarus_fortress.png' WHERE id = 'cov-fudarus-0000';
+UPDATE covenants SET visual_path = '/covenants/cov_exspectatio_cliffs.png' WHERE id = 'cov-exspectatio-0000';
+UPDATE covenants SET visual_path = '/covenants/cov_semitae_weather_tower.png' WHERE id = 'cov-semitae-0000';
+
+UPDATE covenants SET visual_path = '/covenants/cov_harco_villa.png' WHERE id = 'cov-harco-0000';
+UPDATE covenants SET visual_path = '/covenants/cov_favras_citadel.png' WHERE id = 'cov-favras-0000';
+UPDATE covenants SET visual_path = '/covenants/cov_magvillus_palace.png' WHERE id = 'cov-magvillus-0000';
+
+UPDATE covenants SET visual_path = '/covenants/cov_verdi_forge.png' WHERE id = 'cov-verdi-0000';
+UPDATE covenants SET visual_path = '/covenants/cov_twisting_shadows.png' WHERE id = 'cov-twisting-0000';
+
+UPDATE covenants SET visual_path = '/covenants/cov_levrier_manor.png' WHERE id = 'cov-levrier-0000';
+UPDATE covenants SET visual_path = '/covenants/cov_valnastium_palace.png' WHERE id = 'cov-valnastium-0000';
+UPDATE covenants SET visual_path = '/covenants/cov_pythagoranis_schola.png' WHERE id = 'cov-pythagoranis-0000';
+
+UPDATE covenants SET visual_path = '/covenants/cov_jaferia_palace.png' WHERE id = 'cov-jaferia-0000';
+UPDATE covenants SET visual_path = '/covenants/cov_castrasolis_arena.png' WHERE id = 'cov-castrasolis-0000';
+
+UPDATE covenants SET visual_path = '/covenants/cov_acre_crypt.png' WHERE id = 'cov-acre-0000';
